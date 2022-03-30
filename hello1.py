be7325dc35bfbf962cfe9875191c87f17435b2e3
Noudjichris <noudjichrist87@gmail.com>
2022-03-30 12:36:56 +0100

    Adding new method marcher

--- hello1.py
+++ hello1.py
@@ -5,5 +5,8 @@ from msilib.schema import Class
 class Human():
     def parler(self):
         print("Human can speak")
+
+    def marcher(self):
+        print("Human can walk")
 h1 = Human()
 h1.parler()
