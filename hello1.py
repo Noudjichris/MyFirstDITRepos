d8b75073b64462fb73943046270d4edc93e0a966
Noudjichris <noudjichrist87@gmail.com>
2022-03-30 12:50:41 +0100

    Adding new breath method

--- hello1.py
+++ hello1.py
@@ -8,5 +8,9 @@ class Human():
 
     def marcher(self):
         print("Human can walk")
+    def respirer(self):
+        print("Human can breath")
 h1 = Human()
 h1.parler()
+h1.marcher()
+h1.respirer()
